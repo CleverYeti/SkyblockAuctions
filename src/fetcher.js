16107519ce06bf558e14373acf511d7d455d0d60
Olliver Roberge <ollivier.roberge@gmail.com>
2022-05-04 19:31:18 -0400

    tabs > spaces && lf > crlf

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,14 +1,14 @@
-/*
+﻿/*
 "use strict";
 fetch("https://api.hypixel.net/skyblock/auctions").then(response => response.json()).then((json) => {
-    let auctions = json["auctions"];
-    console.log(json["totalPages"]);
-    // TODO: something useful with auctions
-    const tiers = new Map();
-    for (const auction of auctions) {
+	let auctions = json["auctions"];
+	console.log(json["totalPages"]);
+	// TODO: something useful with auctions
+	const tiers = new Map();
+	for (const auction of auctions) {
 
-    }
-    console.log(auctions[69]["tier"]);
+	}
+	console.log(auctions[69]["tier"]);
 }).catch(err => console.log(err));
 
 */
