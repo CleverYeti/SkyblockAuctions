517bece8dc4ee6e201767bff0c0ea4b03778f07a
cleveryeti <cleveryeti3@gmail.com>
2022-05-05 18:23:18 -0400

    added parameters and cleaned up comments

--- backend/backend.py
+++ backend/backend.py
@@ -1,34 +1,43 @@
 
+# amount of auction per page
+page_length = 36
+
+# if the api keys is invalid just put a new one
+# each api key has a max of 65 requests per hour
+# the api keys need to be made by 2 different minecraft accounts
+main_api_key = ''
+second_api_key = ''
+
 # function that reloads the entire data
-def reload():
-    print('reload function called')
+def reloadauctions():
+    print('reloadauctions function called')
     fetchauctions()
     sortauctions()
     copyprocesseddata()
 
-#fetch auction pages from the hypixel api
+# fetch auction pages from the hypixel api
 def fetchauctions():
     print('fetchauctions function called')
-    #fetch all auction pages from the hypixel api
+    # fetch all auction pages from the hypixel api
 
-#sort the auctions by price and end time
+# sort the auctions by price and end time
 def sortauctions():
     print('sortauctions function called')
-    #sort the data into two lists, sorted by price and end time
+    # sort the data into two lists, sorted by price and end time
 
-#copy the processed data into the active lists
+# copy the processed data into the active lists
 def copyprocesseddata():
     print('copyprocesseddata function called')
-    #copy the newly processed data into the active lists
+    # copy the newly processed data into the active lists
 
 
-#filter the auctions for a request
-def filterauctions(page, sort, category, search, rarity, binfilter):
+# filter the auctions for a request
+def filterauctions(amount, sort, category, search, rarity, binfilter):
     print('filterauctions function called')
-    #filter the 
+    # filter the auctions with the inputted properties
 
 
-reload()
+reloadauctions()
 
 print('done')
 
